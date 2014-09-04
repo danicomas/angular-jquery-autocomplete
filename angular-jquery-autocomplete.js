4b8369c6cf32d8c9cea025a17236a6432858d222
danicomas <danicomas@gmail.com>
2014-09-04 14:26:15 +0200

    Added JavaScript

---
+++ angular-jquery-autocomplete.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+angular.module('angular-jquery-autocomplete', []).directive('autocomplete', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            autocompleteconfig: '='
+        },
+        link: function (scope, elem, attr, ctrl) {
+            scope.$watch('autocompleteconfig', function (value) {
+                elem.autocomplete(value);
+            });
+        }
+    };
+});
